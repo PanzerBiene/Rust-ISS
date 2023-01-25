6e77b5e4687636db2b3d8f1f5cd33287b58d27e3
PanzerBiene <panzerbiene@protonmail.com>
2023-01-25 16:39:11 +1100

    removed gui for now, implemented get_pos

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,12 @@
-use druid::widget::{Align, Label, Flex, TextBox};
-use druid::{AppLauncher, Data, Lens, Env, LocalizedString, Widget, WindowDesc, WidgetExt};
 use reqwest;
 use serde_json::{Value};
 
-const VERTICAL_WIDGET_SPACING: f64 = 20.0;
-const TEXT_BOX_WIDTH: f64 = 200.0;
-
-fn main() {
-    //main Window 
-    let main_window = WindowDesc::new(build_root_widget)
-        .title("Rust-ISS")
-        .window_size((400.0, 400.0));
-
-    //start application
-    AppLauncher::with_window(main_window)
-        .launch("".to_owned())
-        .expect("Failed to launch Rust ISS");
+#[tokio::main]
+async fn main() {
+    get_pos().await;
 }
 
-fn build_root_widget() -> impl Widget<String> {
-
-    //textbox
-    let textbox = TextBox::new()
-        .with_placeholder("ISS Location")
-        .fix_width(TEXT_BOX_WIDTH);
-
-    //widget layout
-    let layout = Flex::column()
-        .with_spacer(VERTICAL_WIDGET_SPACING)
-        .with_child(textbox);
-
-    //center 2 widgets in with_space 
-    Align::centered(layout)
-}
-
-async fn get_pos() -> Value {
+async fn get_pos()  {
     let url = "http://api.open-notify.org/iss-now.json";
     let client = reqwest::Client::new();
     let response = client.get(url)
@@ -48,13 +20,12 @@ async fn get_pos() -> Value {
     match response {
         Ok(json) => {
            output = serde_json::from_str(&json).unwrap(); 
+           println!("{:?}", output)
         }
         Err(e) => {
-            output = Value::Null;
             println!("{:?}", e);
         }
     }
 
-    return output;
 
 }
